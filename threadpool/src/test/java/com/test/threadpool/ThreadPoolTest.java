d7ec1afde605d68b3d86b8c84757b1e5f4245025
thbspan <thbspan@163.com>
2023-08-16 11:48:05 +0800

    update ThreadPool test

--- threadpool/src/test/java/com/test/threadpool/ThreadPoolTest.java
+++ threadpool/src/test/java/com/test/threadpool/ThreadPoolTest.java
@@ -1,23 +1,29 @@
 package com.test.threadpool;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
-import org.junit.jupiter.api.Test;
-
-public class ThreadPoolTest {
+class ThreadPoolTest {
 
     @Test
-    public void testShutdownPoolPolicy() {
-        ExecutorService executorService = new ThreadPoolExecutor(1, 1,
-                0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<>(),
-                Executors.defaultThreadFactory(), (r, executor) -> r.run());
-        executorService.shutdown();
+    void testCustomShutdownPoolPolicy() {
+        Assertions.assertDoesNotThrow(() -> {
+            ExecutorService executorService = new ThreadPoolExecutor(1, 1,
+                    0L, TimeUnit.MILLISECONDS,
+                    new LinkedBlockingQueue<>(),
+                    Executors.defaultThreadFactory(),
+                    // 自定义任务抛弃策略
+                    (r, executor) -> r.run());
 
-        executorService.submit(() -> System.out.println("test"));
+            executorService.shutdown();
+            // 线程池关闭后，新添加的任务会执行任务抛弃策略
+            executorService.submit(() -> System.out.println("test"));
+        });
     }
 }
