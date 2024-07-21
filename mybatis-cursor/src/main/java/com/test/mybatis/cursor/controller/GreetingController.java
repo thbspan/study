b2fbe73ea96bd37ffcf2e3eab46f8ae1908a3673
thbspan <thbspan@163.com>
2024-07-21 22:09:20 +0800

    update dependencies version

--- mybatis-cursor/src/main/java/com/test/mybatis/cursor/controller/GreetingController.java
+++ mybatis-cursor/src/main/java/com/test/mybatis/cursor/controller/GreetingController.java
@@ -1,19 +1,20 @@
 package com.test.mybatis.cursor.controller;
 
+import com.test.mybatis.cursor.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.test.mybatis.cursor.service.UserService;
-
-@Controller
+@RestController
 public class GreetingController {
+
     @Autowired
     private UserService userService;
+
     @RequestMapping("/greeting")
-    public String greeting(String name, Model m){
-        m.addAttribute("name",name);
+    public String greeting(String name, Model m) {
+        m.addAttribute("name", name);
         userService.testCursor();
         return "greeting";
     }
